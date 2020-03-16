6c191ed3a43a81c76b7341d37757bda0ac2cf475
ybebee9 <ybebee9@gmail.com>
2020-03-17 00:04:44 +0900

    mutation 테스트

---
+++ graphql/resolvers.js
@@ -0,0 +1,15 @@
+// Query를 해결하는 Resolver
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
+
+const resolvers = {
+    Query: {
+        movies: () => getMovies(),
+        movie: (_, { id }) => getById(id)
+    },
+    Mutation: {
+        addMovie: (_,{name, score}) => addMovie(name,score),
+        deleteMovie: (_,{id}) => deleteMovie(id)
+    }
+};
+
+export default resolvers;
