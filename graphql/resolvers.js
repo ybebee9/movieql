d71b2bbea694287749f109617eb900fcd86cc63b
ybebee9 <ybebee9@gmail.com>
2020-03-17 00:54:11 +0900

    영화API 만들기 완성

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,9 @@
 // Query를 해결하는 Resolver
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
     Query: {
-        movies: () => getMovies(),
-        movie: (_, { id }) => getById(id)
-    },
-    Mutation: {
-        addMovie: (_,{name, score}) => addMovie(name,score),
-        deleteMovie: (_,{id}) => deleteMovie(id)
+        movies: (_,{ rating, limit }) => getMovies(limit, rating)
     }
 };
 
